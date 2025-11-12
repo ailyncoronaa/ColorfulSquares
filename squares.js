b5f089e070607f5787c5e785a87e29fe939b18f3
ailyncoronaa <ailyncoronaa@gmail.com>
2025-11-12 10:37:51 -0500

    Created addSquare function. Refactored original loop. Attached addSquare function to the button

--- squares.js
+++ squares.js
@@ -1,13 +1,29 @@
 window.onload=function(){
-    var squarearea = document.getElementById("squarearea");
-    
+    var add= document.getElementById("add");
+    add.onclick = addSquare;
     var squareCount = parseInt(Math.random()*21)+30;
-    
     for (let i = 0; i < squareCount; i ++){
+        addSquare();
+   
+}
+function getRandomColor(){
+    var possibilities = "0123456789ABCDEF";
+    var result="#";
+    for(var i = 0; i<6;i++)
+    {
+        result+=possibilities.charAt(parseInt(Math.random()*possibilities.length));
+    }
+    return result;
+}
+
+funtion addSquare(){
+    var squarearea = document.getElementById("squarearea");
     var newSquare = document.createElement('div');
     newSquare.className="square";
     newSquare.style.left = parseInt(Math.random()*651)+"px";
     newSquare.style.top = parseInt(Math.random()*251)+"px";
+    newSquare.stylr.backgroundColor=getRandomColor();
     squarearea.appendChild(newSquare);
     }
+
 }
